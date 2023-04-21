7277a9418b3a388a638b228f63ae30fc99b39d1e
Daniela Trinidad Moraga Inostroza <dmoraga@dcc.uchile.cl>
2023-04-21 11:45:43 -0400

    Add hook implementation (doesn´t work)

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,37 +1,78 @@
 extends CharacterBody2D
 
+var speed = 450
+var gravity = 10
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-const ACCELERATION = 1000
-const GRAVITY = 1000
+var motion = Vector2()
 
-@onready var animation_player = $AnimationPlayer
-@onready var animation_tree = $AnimationTree
-@onready var playback = animation_tree.get("parameters/playback")
-@onready var pivot = $Pivot
+var hook_pos = Vector2()
+var hooked = false
+var rope_length = 500
+var current_rope_length
+
+func _ready():
+	current_rope_length = rope_length
+
+func _gravity():
+	motion.y += gravity
+
+func move(delta):
+	if Input.is_action_pressed("move_right"):
+		motion.x = speed
+	elif Input.is_action_pressed("move_left"):
+		motion.x = -speed
+	else:
+		motion.x = 0
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += GRAVITY * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	var move_input = Input.get_axis("move_left","move_right")
+	print(position)
+	hook()
+	#update()
+	if hooked:
+		swing(delta)
+		motion *= 0.975 #speed of swing
+		velocity = motion
+		move_and_slide()
 		
-	velocity.x = move_toward(velocity.x, move_input * SPEED, ACCELERATION * delta)
-
+	_gravity()
+	move(delta)
+	velocity = motion
 	move_and_slide()
 
-	# Animation
-
-	if abs(velocity.x) > 10:
-		playback.travel("run")
+func _draw():
+	var pos = global_position
+	
+	if hooked:
+		draw_line(Vector2(0,0), to_local(hook_pos), Color(0,35,0.7,0.9),3,true) #cyan
 	else:
-		playback.travel("idle")
+		return
 		
-	if move_input:
-		pivot.scale.x = sign(move_input)
+		var colliding = $Raycast.is_colliding()
+		var collide_point = $Raycast.get_collision_point()
+		if colliding and pos.distance_to(collide_point) > rope_length:
+			draw_line(Vector2(0,0), to_local(collide_point), Color(1,1,1,0.25),0.5,true) #white
+func hook():
+	$Raycast.look_at(get_global_mouse_position())
+	if Input.is_action_just_pressed("left_click"):
+		hook_pos = get_hook_pos()
+		if hook_pos:
+			hooked = true
+			current_rope_length = global_position.distance_to(hook_pos)
+			
+func get_hook_pos():
+	for raycast in $Raycast.get_children():
+		if raycast.is_colliding():
+			return raycast.get_collision_point()
+			
+func swing(delta):
+	var radius = global_position - hook_pos
+	if motion.length() < 0.01 or radius.length() < 10: return
+	var angle = acos(radius.dot(motion) / (radius.length() * motion.length()))
+	var rad_vel = cos(angle) * motion.length()
+	motion += radius.normalized() * -rad_vel
+	
+	if global_position.distance_to(hook_pos) > current_rope_length:
+		global_position =hook_pos + radius.normalized() * current_rope_length
+		
+	motion += (hook_pos - global_position).normalized() * 15000 * delta
+	
