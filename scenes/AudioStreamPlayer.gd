bf4fda139d1cf7e676aa4cc4daa8204efa716047
Matimcs <matiassanhueza.ms@gmail.com>
2023-04-25 18:27:10 -0400

    Song Add and Menu upgrade
    Solomon, HWV 67 (Handel, George Frideric)

---
+++ scenes/AudioStreamPlayer.gd
@@ -0,0 +1,20 @@
+extends AudioStreamPlayer
+
+var songs = ["BWV_1043.mp3", "Solomon, HWV 67 (Handel, George Frideric).mp3"]
+var last_song = -1
+
+func play_random_song():
+	var next_song = randi() % songs.size()
+	if next_song == last_song:
+		next_song = (next_song + 1) % songs.size()
+	last_song = next_song
+	var song_path = "res://Música/In-Game/" + songs[next_song]
+	stream = load(song_path)
+	play()
+
+func _ready():
+	songs.shuffle()
+	play_random_song()
+
+func _on_finished():
+	play_random_song()
