9530112fddef03fe8dba7d72fcc6b034f9ce861f
fercontreras10 <90367308+fercontreras10@users.noreply.github.com>
2023-07-20 01:54:51 -0400

    fix error tween

--- scenes/banana_2d.gd
+++ scenes/banana_2d.gd
@@ -7,13 +7,13 @@ func _on_body_entered(body):
 	#body.scale *= Vector2(1.2, 1.2)
 	
 	if body.name == "Player":
-		var tween = get_tree().create_tween()
-		var tween1 = get_tree().create_tween()
+		var tween = create_tween()
+		var tween1 = create_tween()
 		tween.set_trans(Tween.TRANS_CIRC)
 		tween.set_ease(Tween.EASE_OUT)
 		tween.tween_property(self, "position", position - Vector2(0, 150).rotated(deg_to_rad(-45)), 0.3)
 		tween1.tween_property(self, "modulate:a", 0, 0.3)
 		tween.tween_callback(queue_free)
 		tween1.tween_callback(queue_free)
-		#queue_free()
 		body.update_banana_num()
+
